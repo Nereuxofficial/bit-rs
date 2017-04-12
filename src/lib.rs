350da468d88f325968f074d63ce0dbbb3a783b32
José Miguel Sánchez García <jmi2k@openmailbox.org>
2017-04-12 15:26:15 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,58 @@
+//! This crate adds simple bit-manipulation helpers in a convenient trait.
+//! Also, it implements them for the unsigned primitive types.
+
 #![no_std]
+#![warn(missing_docs)]
 
 #[cfg(test)]
 mod tests;
 
 use core::ops::Range;
 
+/// A trait which provides methods for manipulating bits or bit ranges.
 pub trait BitIndex {
+    /// Length of the implementor type in bits.
     fn bit_length() -> usize;
+
+    /// Obtains the value of the bit at the given position, being 0 the least
+    /// significant bit.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if the index is out of bounds, e.g: `pos >=
+    /// Self::bit_length()`.
     fn bit(&self, pos: usize) -> bool;
+
+    /// Obtains the value of the bits inside the given range, being 0 the least
+    /// significant bit.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if:
+    ///
+    ///  - Range `start` is equal or higher than `end`
+    ///  - Range `end` is out of bounds, e.g: `pos.end > Self::bit_length()`
     fn bit_range(&self, pos: Range<usize>) -> Self;
+
+    /// Sets the value of the bit at the given position, being 0 the least
+    /// significant bit.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if the index is out of bounds, e.g: `pos >=
+    /// Self::bit_length()`.
     fn set_bit(&mut self, pos: usize, val: bool) -> &mut Self;
+
+    /// Sets the value of the bits inside the given range, being 0 the least
+    /// significant bit.
+    ///
+    /// # Panics
+    ///
+    /// This method will panic if:
+    ///
+    ///  - Range `start` is equal or higher than `end`
+    ///  - Range `end` is out of bounds, e.g: `pos.end > Self::bit_length()`
+    ///  - Value doesn't fit in the given range
     fn set_bit_range(&mut self, pos: Range<usize>, val: Self) -> &mut Self;
 }
 
